4a4260b9acdda73329369adaf354c83d8dad7034
Brent <Bannawit.KH@gmail.com>
2021-03-22 19:43:40 +0700

    develop site: revision 4

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
 //this part is the shrinking navbar src:https://www.w3schools.com/howto/howto_js_navbar_shrink_scroll.asp/
-viewConfig: {
-    loadingText: lang.loading,
-    loadMask: true,
-    stripeRows: true,
-    getRowClass: function(record, rowIndex, rowParams, store) {
-        return 'multiline-row';
-    }
-},
